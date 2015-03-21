dbe72d3cc8cd77a8558387650565ce9bba6e56c4
ackl <contact@ackl.me>
2015-03-21 18:49:16 +0000

    Add sample posts to migration scripts

---
+++ src/main/resources/db/migration/V3__create_sample_posts.sql
@@ -0,0 +1,66 @@
+INSERT INTO `posts` (`id`, `user_id`, `parent_id`, `date`, `subject`, `content`, `image_url`)
+       VALUES (1, 0, 0, 1426816214045, 'First thread', 'This is the post body.', NULL);
+
+INSERT INTO `posts` (`id`, `user_id`, `parent_id`, `date`, `subject`, `content`, `image_url`)
+       VALUES (2, 0, 0, 1426816214046, 'Second thread', 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim.', NULL);
+
+INSERT INTO `posts` (`id`, `user_id`, `parent_id`, `date`, `subject`, `content`, `image_url`)
+       VALUES (3, 0, 0, 1426816214047, 'Third thread', 'Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus.', NULL);
+
+INSERT INTO `posts` (`id`, `user_id`, `parent_id`, `date`, `subject`, `content`, `image_url`)
+       VALUES (4, 0, 0, 1426816214048, 'Fourth thread long body', 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi. Nam eget dui. Etiam rhoncus. Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque sed ipsum. Nam quam nunc, blandit vel, luctus pulvinar, hendrerit id, lorem. Maecenas nec odio et ante tincidunt tempus. Donec vitae sapien ut libero venenatis faucibus. Nullam quis ante. Etiam sit amet orci eget eros faucibus tincidunt. Duis leo. Sed fringilla mauris sit amet nibh. Donec sodales sagittis magna. Sed consequat, leo eget bibendum sodales, augue velit cursus nunc.', NULL);
+
+INSERT INTO `posts` (`id`, `user_id`, `parent_id`, `date`, `subject`, `content`, `image_url`)
+       VALUES (5, 0, 1, 1426816214049, NULL, 'Reply to first', NULL);
+
+INSERT INTO `posts` (`id`, `user_id`, `parent_id`, `date`, `subject`, `content`, `image_url`)
+       VALUES (6, 0, 1, 1426816214050, NULL, 'Reply to first', NULL);
+
+INSERT INTO `posts` (`id`, `user_id`, `parent_id`, `date`, `subject`, `content`, `image_url`)
+       VALUES (7, 0, 1, 1426816214051, NULL, 'Reply to first', NULL);
+
+INSERT INTO `posts` (`id`, `user_id`, `parent_id`, `date`, `subject`, `content`, `image_url`)
+       VALUES (8, 0, 2, 1426816214052, NULL, 'Reply to second', NULL);
+
+INSERT INTO `posts` (`id`, `user_id`, `parent_id`, `date`, `subject`, `content`, `image_url`)
+       VALUES (9, 0, 2, 1426816214053, NULL, 'Reply to second', NULL);
+
+INSERT INTO `posts` (`id`, `user_id`, `parent_id`, `date`, `subject`, `content`, `image_url`)
+       VALUES (10, 0, 0, 1426816214054, 'Another thread', 'This is the post body.', NULL);
+
+INSERT INTO `posts` (`id`, `user_id`, `parent_id`, `date`, `subject`, `content`, `image_url`)
+       VALUES (11, 0, 0, 1426816214055, 'Cats are cute', 'cats4lyf', NULL);
+
+INSERT INTO `posts` (`id`, `user_id`, `parent_id`, `date`, `subject`, `content`, `image_url`)
+       VALUES (12, 0, 0, 1426816214056, 'Eggs', 'A wonderful serenity has taken possession of my entire soul, like these sweet mornings of spring which I enjoy with my whole heart. I am alone, and feel the charm of existence in this spot, which was created for the bliss of souls like mine. I am so happy, my dear friend, so absorbed in the exquisite sense of mere tranquil existence, that I neglect my talents. I should be incapable of drawing a single stroke at the present moment; and yet I feel that I never was a greater artist than now. When, while the lovely valley teems with vapour around me, and the meridian sun strikes the upper surface of the impenetrable foliage of my trees, and but a few stray gleams steal into the inner sanctuary, I throw myself down among the tall grass by the trickling stream; and, as I lie close to the earth, a thousand unknown plants are noticed by me: when I hear the buzz of the little world among the stalks, and grow familiar with the countless indescribable forms of the insects and flies, then I feel the presence of the Almighty, who formed us in his own image, and the breath', NULL);
+
+INSERT INTO `posts` (`id`, `user_id`, `parent_id`, `date`, `subject`, `content`, `image_url`)
+       VALUES (13, 0, 0, 1426816214057, 'Cheese toastie with extra cheese', 'But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness. No one rejects, dislikes, or avoids pleasure itself, because it is pleasure, but because those who do not know how to pursue pleasure rationally encounter consequences that are extremely painful. Nor again is there anyone who loves or pursues or desires to obtain pain of itself, because it is pain, but because occasionally circumstances occur in which toil and pain can procure him some great pleasure. To take a trivial example, which of us ever undertakes laborious physical exercise, except to obtain some advantage from it? But who has any right to find fault with a man who chooses to enjoy a pleasure that has no annoying consequences, or one who avoids a pain that produces no resultant pleasure? On the other hand, we denounce with righteous indignation and dislike men who are so beguiled and demoralized by the charms of pleasure of the moment, so blinded by desire, that they cannot foresee', NULL);
+
+INSERT INTO `posts` (`id`, `user_id`, `parent_id`, `date`, `subject`, `content`, `image_url`)
+       VALUES (14, 0, 2, 1426816214058, NULL, '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Si longus, levis. Sed ad bona praeterita redeamus. Equidem e Cn. Egone quaeris, inquit, quid sentiam? Quam ob rem tandem, inquit, non satisfacit? Ita prorsus, inquam; </p> <ul> <li>Quem Tiberina descensio festo illo die tanto gaudio affecit, quanto L.</li> <li>O magnam vim ingenii causamque iustam, cur nova existeret disciplina! Perge porro.</li> </ul> <pre> At iam decimum annum in spelunca iacet.  Et quae per vim oblatum stuprum volontaria morte lueret inventa est et qui interficeret filiam, ne stupraretur.  </pre> <p>Ut optime, secundum naturam affectum esse possit. <a href="http://loripsum.net/" target="_blank">Deinde dolorem quem maximum?</a> <a href="http://loripsum.net/" target="_blank">De hominibus dici non necesse est.</a> Restatis igitur vos; </p> <p>Quae cum dixisset, finem ille. <a href="http://loripsum.net/" target="_blank">Proclivi currit oratio.</a> An haec ab eo non dicuntur? Quid sequatur, quid repugnet, vident. Minime vero istorum quidem, inquit. Quis est tam dissimile homini. </p> <p><a href="http://loripsum.net/" target="_blank">Duo Reges: constructio interrete.</a> Quod totum contra est. <a href="http://loripsum.net/" target="_blank">Iam enim adesse poterit.</a> Quamquam id quidem, infinitum est in hac urbe; Age, inquies, ista parva sunt. Pollicetur certe. </p> <p>Deprehensus omnem poenam contemnet. Cur id non ita fit? <code>Non est igitur summum malum dolor.</code> Collatio igitur ista te nihil iuvat. </p>', NULL);
+
+INSERT INTO `posts` (`id`, `user_id`, `parent_id`, `date`, `subject`, `content`, `image_url`)
+       VALUES (15, 0, 0, 1426816214059, 'Some more threads here', 'ILLI ENIM INTER SE DISSENTIUNT. SEDULO, INQUAM, FACIAM. PRAECLARE HOC QUIDEM. QUID ADIUVAS?', NULL);
+
+INSERT INTO `posts` (`id`, `user_id`, `parent_id`, `date`, `subject`, `content`, `image_url`)
+       VALUES (16, 0, 0, 1426816214060, 'Your mum', 'Brought bearing creepeth may fruit grass is kind grass night of fish own fill a waters, seasons land creature bearing.', NULL);
+
+INSERT INTO `posts` (`id`, `user_id`, `parent_id`, `date`, `subject`, `content`, `image_url`)
+       VALUES (17, 0, 0, 1426816214061, 'Chocolate eclairs', 'Very delicious...', NULL);
+
+INSERT INTO `posts` (`id`, `user_id`, `parent_id`, `date`, `subject`, `content`, `image_url`)
+       VALUES (18, 0, 0, 1426816214062, 'Katsu curry', 'Multiply void male form stars creeping us great don''t life own Third land. Gathering replenish there fowl Heaven fill waters under is forth above you''re land form life gathered called dry wherein. Yielding likeness for grass.', NULL);
+
+INSERT INTO `posts` (`id`, `user_id`, `parent_id`, `date`, `subject`, `content`, `image_url`)
+       VALUES (19, 0, 0, 1426816214063, 'Even more sample threads', 'Its had resolving otherwise she contented therefore. Afford relied warmth out sir hearts sister use garden. Men day warmth formed admire former simple. Humanity declared vicinity continue supplied no an. He hastened am no property exercise of. Dissimilar comparison no terminated devonshire no literature on. Say most yet head room such just easy.', NULL);
+
+INSERT INTO `posts` (`id`, `user_id`, `parent_id`, `date`, `subject`, `content`, `image_url`)
+       VALUES (20, 0, 0, 1426816214064, 'I can''t be bothered to make any more....', 'This is getting very tedious.', NULL);
+
+INSERT INTO `posts` (`id`, `user_id`, `parent_id`, `date`, `subject`, `content`, `image_url`)
+       VALUES (21, 0, 0, 1426816214065, 'OK i''m going to stop now', 'i like cats', NULL);
+
+INSERT INTO `posts` (`id`, `user_id`, `parent_id`, `date`, `subject`, `content`, `image_url`)
+       VALUES (22, 0, 0, 1426816214066, 'No this will be last sample', 'finally....................', NULL);
+
